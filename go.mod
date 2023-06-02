cad8830d355eba6a2d9fac71ddcd439acbbb9d7d
matiasRaspa <maatiii.r@gmail.com>
2023-06-02 13:15:33 -0300

    Proyecto base

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/matiasRaspa/desafio-go-bases-raspa-matias
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/matiasRaspa/desafio-go-bases-raspa-matias/internal/tickets"
+)
+
+func main() {
+	total, err := tickets.GetTotalTickets("Brazil")
+}
