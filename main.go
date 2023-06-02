5f69e4fe7ce9ca10240fdc177ec1b9f4d9704528
matiasRaspa <maatiii.r@gmail.com>
2023-06-02 14:09:29 -0300

    Requerimiento 1

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"github.com/matiasRaspa/desafio-go-bases-raspa-matias/internal/tickets"
+	"log"
 )
 
 func main() {
-	total, err := tickets.GetTotalTickets("Brazil")
+	
+	//Cargo datos
+	listaTickets, err := tickets.CargarTickets("tickets.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Compruebo datos
+	fmt.Println(listaTickets[0].PaisDestino)
+
+	//Requerimiento 1
+	totalPersonasPorDestino, destino, err := tickets.ContarPersonasPorDestino(listaTickets, "Australia")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//Muestro total
+	fmt.Printf("\n############### Requerimiento 1 ###############\nEl total de personas que viajan a %s es: %d\n", destino, totalPersonasPorDestino)
 }
